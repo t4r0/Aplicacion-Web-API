dc4908b82b2b671a3ee0be00498c12658393f2f5
Yesinoemi <yesicargueta@gmail.com>
2016-09-14 00:37:45 -0600

    Creacion vista consejo
    Signed-off-by: Yesinoemi <yesicargueta@gmail.com>

---
+++ APITX/app/vistas/vistaConsejo.py
@@ -0,0 +1,47 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from app.models import TxcoConsejo
+from app.serializables import TxcoConsejoS
+
+
+@api_view(['GET', 'POST'])
+def lista_objetos(request):
+    """
+    Lista de todas los consejos, o crear uno nuevo
+    """
+    if request.method == 'GET':
+        objeto = TxcoConsejo.objects.all()
+        serializador = TxcoConsejoS(objeto, many=True)
+        return Response(serializador.data)
+
+    elif request.method == 'POST':
+        serializador = TxcoConsejoS(data=request.data)
+        if serializador.is_valid():
+            serializador.save()
+            return Response(serializador.data,status=status.HTTP_201_CREATED)
+            return Response(serializador.errors,status=status.HTTP_400_BAD_REQUEST)
+@api_view(['GET', 'PUT','DELETE'])
+def detalle_objetos(request, pk):
+    """
+    Actualiza, elimina un objeto segun su id
+    """
+    try:
+        objeto = TxcoConsejo.objects.get(pk=pk)
+    except objeto.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializador = TxcoConsejoS(objeto)
+        return Response(serializador.data)
+
+    elif request.method == 'PUT':
+        serializador = TxcoConsejoS(objeto, data=request.data)
+        if serializador.is_valid():
+            serializador.save()
+            return Response(serializador.data)
+        return Response(serializador.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        objeto.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
