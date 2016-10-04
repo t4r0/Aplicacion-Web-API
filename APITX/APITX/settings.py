c1ef4f17ad47c872ea1f2500c0ec41d1493e6007
Estuardo Diaz <esjoando555@gmail.com>
2016-10-03 21:33:04 -0600

    Updates settings for sqlite

--- APITX/APITX/settings.py
+++ APITX/APITX/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-import pymysql
-pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -91,12 +89,8 @@ WSGI_APPLICATION = 'APITX.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dbTransXela',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'mydb.db'),
     }
 }
 
